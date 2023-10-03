b2330f53b5e89aef9ab52f6e9ef08685a662538f
Ben-David <samuelbendavid01@gmail.com>
2023-10-03 09:27:57 +0100

    second commit

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
 const app = express();
+const socket = require("socket.io");
 const PORT = 3000 ||  process.env.PORT;
 
+const server = app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
+
 app.use(express.static("public"));
+app.use('/lib', express.static(__dirname + '/lib'));
 
 // Template Engine
 app.use(expressLayout);
@@ -16,6 +22,25 @@ app.set("view engine", "ejs")
 //Route setup
 app.use(("/"), require("./server/routes/main"));
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-});
+// Socket setup
+const io = socket(server);
+
+const activeUsers = new Set();
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+  
+    socket.on("new user", function (data) {
+      socket.userId = data;
+      activeUsers.add(data);
+      io.emit("new user", [...activeUsers]);
+    });
+  
+    socket.on("disconnect", function () {
+        activeUsers.delete(socket.userId);
+        io.emit("user disconnected", socket.userId);
+      });
+  
+      socket.on("chat message", function (data) {
+        io.emit("chat message", data);
+    });
+  });
--- public/js/main.js
+++ public/js/main.js
@@ -29,17 +29,128 @@
     });
     
     
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
 })(jQuery);
+
+
+const socket = io();
+
+const inboxPeople = document.querySelector(".inbox__people");
+
+const newUserConnected = function (data) {
+    
+
+  //give the user a random unique id
+  id = Math.floor(Math.random() * 1000000);
+  userName = 'user-' +id;
+  //console.log(typeof(userName));   
+  
+
+  //emit an event with the user id
+  socket.emit("new user", userName);
+  //call
+  addToUsersBox(userName);
+};
+
+const addToUsersBox = function (userName) {
+    //This if statement checks whether an element of the user-userlist
+    //exists and then inverts the result of the expression in the condition
+    //to true, while also casting from an object to boolean
+    if (!!document.querySelector(`.${userName}-userlist`)) {
+        return;
+    }
+    
+    //setup the divs for displaying the connected users
+    //id is set to a string including the username
+    const userBox = `
+    <div class="chat_id ${userName}-userlist">
+      <h5>${userName}</h5>
+    </div>
+  `;
+    //set the inboxPeople div with the value of userbox
+    inboxPeople.innerHTML += userBox;
+};
+
+//call 
+newUserConnected();
+
+//when a new user event is detected
+socket.on("new user", function (data) {
+  data.map(function (user) {
+          return addToUsersBox(user);
+      });
+});
+
+//when a user leaves
+socket.on("user disconnected", function (userName) {
+  // Remove the user from the user list (your existing code)
+  document.querySelector(`.${userName}-userlist`).remove();
+
+  // Display a disconnection message
+  const disconnectionMessage = `${userName} has left the chat.`;
+  const disconnectionMessageElement = document.getElementById("disconnection-message");
+  
+  // Update the content of the disconnection message element
+  disconnectionMessageElement.textContent = disconnectionMessage;
+});
+
+// When a new user joins
+socket.on("new user", function (userName) {
+  // Display a welcome message
+  const welcomeMessage = `${userName} has joined the chat.`;
+  const welcomeMessageElement = document.getElementById("welcome-message");
+  
+  // Update the content of the welcome message element
+  welcomeMessageElement.textContent = welcomeMessage;
+});
+
+
+const inputField = document.querySelector(".message_form__input");
+const messageForm = document.querySelector(".message_form");
+const messageBox = document.querySelector(".messages__history");
+
+const addNewMessage = ({ user, message }) => {
+  const time = new Date();
+  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+
+  const receivedMsg = `
+  <div class="incoming__message">
+    <div class="received__message">
+      <p>${message}</p>
+      <div class="message__info">
+        <span class="message__author">${user}</span>
+        <span class="time_date">${formattedTime}</span>
+      </div>
+    </div>
+  </div>`;
+
+  const myMsg = `
+  <div class="outgoing__message">
+    <div class="sent__message">
+      <p>${message}</p>
+      <div class="message__info">
+        <span class="time_date">${formattedTime}</span>
+      </div>
+    </div>
+  </div>`;
+
+  //is the message sent or received
+  messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
+};
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputField.value) {
+    return;
+  }
+
+  socket.emit("chat message", {
+    message: inputField.value,
+    nick: userName,
+  });
+
+  inputField.value = "";
+});
+
+socket.on("chat message", function (data) {
+  addNewMessage({ user: data.nick, message: data.message });
+});
